55525687dbb202473e5b2ae326e9a471aafbd8bd
Viktor Magnusson <viktor.kuddis@hotmail.com>
2023-12-16 00:54:16 +0100

    refactor and comments

--- js/clockAndDate_function.js
+++ js/clockAndDate_function.js
@@ -1,30 +1,4 @@
-
-
 function clockAndDate() {
-
-    const nameOfMonths = [
-        "januari",
-        "februar",
-        "mars",
-        "april",
-        "maj",
-        "juni",
-        "juli",
-        "augusti",
-        "september",
-        "oktober",
-        "noveber",
-        "december"
-    ];
-    const nameOfDay = [
-        "söndag",
-        "måndag",
-        "tisdag",
-        "onsdag",
-        "torsdag",
-        "fredag",
-        "lördag"
-    ];
 
     // timme, minut datum månad år
     const clock = new Date();
@@ -32,7 +6,7 @@ function clockAndDate() {
     //bryter ut klockan i delar:
     let hour = clock.getHours().toString().padStart(2, "0");
     let min = clock.getMinutes().toString().padStart(2, "0");
-    let sec = clock.getSeconds().toString().padStart(2, "0");
+    // let sec = clock.getSeconds().toString().padStart(2, "0");
     let day = clock.getDay();
     let date = clock.getDate();
     let month = clock.getMonth();
@@ -40,10 +14,8 @@ function clockAndDate() {
 
     //console.log(`${hour}:${min}:${sec}   ${nameOfDay[day]} ${date} ${nameOfMonths[month]} ${year}`);
 
-    //skapar en string av mina datumkomponenter
+    //skapar en string av mina datumkomponenter och matar in i domen
     timeContainer.innerHTML = `${hour}:${min}`;
-
-
     dateContainer.innerHTML = `${nameOfDay[day]} ${date} ${nameOfMonths[month]} ${year}`;
 
 }
