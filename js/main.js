f28e1b9e56a6d459db7fa281330fea13aec6a3ef
Viktor Magnusson <viktor.kuddis@hotmail.com>
2023-12-16 00:09:06 +0100

    clock function i working

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+const dateAndTime_section = document.getElementById("date-and-time_section");
+const timeContainer = dateAndTime_section.querySelector("#time-container");
+const dateContainer = dateAndTime_section.querySelector("#date-container");
+
+setInterval(clockAndDate, 1000);
+
+
+
