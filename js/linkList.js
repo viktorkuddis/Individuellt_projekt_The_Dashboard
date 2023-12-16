ecc22ca45040ccd534c4ff08f8c51d6fbf32e7ba
Viktor Magnusson <viktor.kuddis@hotmail.com>
2023-12-16 22:11:27 +0100

    addlink style

--- js/linkList.js
+++ js/linkList.js
@@ -2,17 +2,17 @@ const addLink_btn = document.getElementById("add-link_btn");
 const snabblänkar_activityCard = document.getElementById("snabblänkar_activity-card")
 
 const inputComponent = `
-<div>
+<div id="add-link_component">
 
-    <p>Lägg till länk</p>
-    <input type="text" id="sidrubrik_input" placeholder="Sidrubrik här">
-    <input type="text" id="webbadress_input" placeholder="Webbadress här">
-    
-    <div>
-        <div>
-            <button id="confirm-add-Link_btn">Lägg till</button>
-            <button id="exit-add-Link_btn">&#10005</button>
-        </div>
+    <div id="add-link-input_container">
+        <p><strong>Lägg till länk: </strong></p>
+        <input type="text" id="sidrubrik_input" placeholder="Sidrubrik här">
+        <input type="text" id="webbadress_input" placeholder="Webbadress här">
+    </div>
+
+    <div id="add-link-buttons_container">
+        <button id="confirm-add-Link_btn">Lägg till</button>
+        <button id="exit-add-Link_btn">&#10005</button>
     </div>
 
 </div>
