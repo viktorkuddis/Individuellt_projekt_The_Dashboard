7894bccda4b28d39caa6d90b9cbfd144e36b8c5c
Viktor Magnusson <viktor.kuddis@hotmail.com>
2023-12-16 18:32:54 +0100

    refactor clockfunktion

--- js/clockAndDate_function.js
+++ js/clockAndDate_function.js
@@ -1,3 +1,32 @@
+//hämtar element från dom
+const dateAndTime_section = document.getElementById("date-and-time_section");
+const timeContainer = dateAndTime_section.querySelector("#time-container");
+const dateContainer = dateAndTime_section.querySelector("#date-container");
+// definerar månader och dagarnas namn åt funktionen
+const nameOfMonths = [
+    "januari",
+    "februar",
+    "mars",
+    "april",
+    "maj",
+    "juni",
+    "juli",
+    "augusti",
+    "september",
+    "oktober",
+    "noveber",
+    "december"
+];
+const nameOfDay = [
+    "söndag",
+    "måndag",
+    "tisdag",
+    "onsdag",
+    "torsdag",
+    "fredag",
+    "lördag"
+];
+
 function clockAndDate() {
 
     // timme, minut datum månad år
