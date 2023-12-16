ae0e54b8baf86955f2d60a368a8feb6f02a1b700
Viktor Magnusson <viktor.kuddis@hotmail.com>
2023-12-17 00:13:38 +0100

    hämtar input och lagrar i local storage

--- js/linkList.js
+++ js/linkList.js
@@ -1,7 +1,15 @@
+/* ----- LÄNKLISTAN ---- */
+//hämtar från local storage eller startar som tom array
+const linksList_array = JSON.parse(localStorage.getItem("linkList")) || []
+/* --------------------- */
+
+
+//Kortet för alla länkar
 const snabblänkar_activityCard = document.getElementById("snabblänkar_activity-card")
 
-// Lagra knapp:
+// Lagra lägg-till-knapp:
 const addLink_btn = document.getElementById("add-link_btn");
+
 
 const inputComponent = `
 <div id="add-link_component">
@@ -20,6 +28,7 @@ const inputComponent = `
 </div>
 `
 
+
 // när initiala knappen klickas...
 addLink_btn.addEventListener("click", () => {
     // initialknapp tars bort...
@@ -27,19 +36,53 @@ addLink_btn.addEventListener("click", () => {
     // inputkomponent läggs till...
     snabblänkar_activityCard.innerHTML += inputComponent;
 
-    //område för knappar:
+    //lagra område för knappar:
     const addLinkButtons_container = document.getElementById("add-link-buttons_container");
-
     //Eventlyssnare på knapparna:
     addLinkButtons_container.addEventListener("click", (event) => {
 
         //om exitknapp trycks på...
         if (event.target.id == "exit-add-Link_btn") {
-            //tar bort hela add-link-komponenten
-            snabblänkar_activityCard.removeChild(document.getElementById("add-link_component"));
-            //Visar initiala knapp igen: 
-            snabblänkar_activityCard.appendChild(addLink_btn);
+            returnFromAddlinkDialog();
         }
 
-    })
+
+        // när lägg-till knapp trycks
+        if (event.target.id == "confirm-add-Link_btn") {
+            console.log("läggtill knapp tryckt")
+
+            const rubrik = document.getElementById("sidrubrik_input").value.trim();
+            const webbadress = document.getElementById("webbadress_input").value.trim();
+
+            //om båda rutorna är ifyllda...
+            if (rubrik.length > 0 && webbadress.length > 0) {
+                console.log("saker sker")
+
+                //Skapar länkobjekt att lagra:
+                const linkObject = {
+                    name: rubrik,
+                    link: webbadress
+                };
+
+                //skickar in i array
+                linksList_array.push(linkObject);
+                //Skickar arrayen in i local storage:
+                localStorage.setItem("linkList", JSON.stringify(linksList_array));
+
+                returnFromAddlinkDialog();
+
+            };
+
+        };
+
+    });
+
 });
+
+
+function returnFromAddlinkDialog() {
+    //tar bort hela add-link-komponenten
+    snabblänkar_activityCard.removeChild(document.getElementById("add-link_component"));
+    //Visar initiala knapp igen: 
+    snabblänkar_activityCard.appendChild(addLink_btn);
+};
