4b2485d4076692542c956d6dad4ddc9f5a47ce13
Viktor Magnusson <viktor.kuddis@hotmail.com>
2023-12-17 18:08:42 +0100

    weather fetch today

---
+++ js/weather.js
@@ -0,0 +1,27 @@
+//Hämtar Geolocation
+navigator.geolocation.getCurrentPosition(successcallback, errorcallback);
+
+function successcallback(position) {
+    console.log(position);
+
+    //detta är nuvarande väder
+    getWeather();
+    async function getWeather() {
+        let res = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=65.3211858&lon=21.4763408&appid=0665c46959a53cecf58adb701dda45c0");
+        console.log(res);
+
+        if (res.ok) {
+            let data = await res.json()
+            console.log(data)
+        }
+    };
+
+
+
+
+
+};
+
+function errorcallback(e) {
+    console.error(e)
+};
