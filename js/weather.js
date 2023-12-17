c25a51936a808e7d4609d67c1abc3d274653ad42
Viktor Magnusson <viktor.kuddis@hotmail.com>
2023-12-17 19:04:08 +0100

    prepared commit

--- js/weather.js
+++ js/weather.js
@@ -4,29 +4,34 @@ navigator.geolocation.getCurrentPosition(successcallback, errorcallback);
 function successcallback(position) {
     console.log(position);
 
-    // //detta är nuvarande väder
-    // getWeather();
-    // async function getWeather() {
-    //     let res = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=65.3211858&lon=21.4763408&appid=0665c46959a53cecf58adb701dda45c0");
-    //     console.log(res);
+    const lon = position.lon;
+    const lat = position.lat;
 
-    //     if (res.ok) {
-    //         let data = await res.json()
-    //         console.log(data)
-    //     }
-    // };
+    const apikey = "0665c46959a53cecf58adb701dda45c0";
 
-    //detta är prognos
-    getWeatherprognos();
-    async function getWeatherprognos() {
-        let res = await fetch("https://api.openweathermap.org/data/2.5/forecast?lat=65.3211858&lon=21.4763408&appid=0665c46959a53cecf58adb701dda45c0");
+    //detta är nuvarande väder
+    getCurrentWeather();
+    async function getCurrentWeather() {
+        let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`);
         console.log(res);
 
         if (res.ok) {
             let data = await res.json()
             console.log(data)
-        } else { (console.log("nåt gick fel")) }
+        } else { console.log("fel") }
     };
+
+    //detta är prognos
+    // getWeatherprognos();
+    // async function getWeatherprognos() {
+    //     let res = await fetch("https://api.openweathermap.org/data/2.5/forecast?lat=65.3211858&lon=21.4763408&appid=0665c46959a53cecf58adb701dda45c0");
+    //     console.log(res);
+
+    //     if (res.ok) {
+    //         let data = await res.json()
+    //         console.log(data)
+    //     } else { (console.log("nåt gick fel")) }
+    // };
 
 
 
