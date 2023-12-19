3bb6429a147f1c68951be4cd501d7602244d2f87
Viktor Magnusson <viktor.kuddis@hotmail.com>
2023-12-19 02:36:16 +0100

    render pokemon img

--- js/buddy.js
+++ js/buddy.js
@@ -10,6 +10,13 @@ Funktionen anropar i sin tur funktioneen som renderar alternativen till domen
 
 
 const buddyList_div = document.getElementById("buddy-list");
+const buddy_container = document.getElementById("buddy_container");
+const buddyReturn_button = document.getElementById("buddy-return_button");
+
+//Döljer return-button initialt:
+buddyReturn_button.style.display = "none";
+
+
 
 let buddys = [];
 
@@ -62,14 +69,56 @@ async function getPokedex() {
 
 
 function renderBudyList() {
-    let buddyListToRender = buddys.map((pokemon) => {
+    let buddyListToRender = buddys.map((pokemon, index) => {
         return `
-            <button class="buddy-list">${pokemon.name.toUpperCase()}</button>`;
+            <button class="buddy-list" id="buddyIndex:${index}">${pokemon.name.toUpperCase()}</button>`;
     });
 
     buddyList_div.innerHTML = buddyListToRender.join("");
 
-    console.log(buddiListToRender);
-
 }
 
+//lyssnar efter knapptryck på buddylistan:
+buddyList_div.addEventListener("click", (event) => {
+    console.log(event);
+
+    if (event.target.nodeName == "BUTTON") {
+        // console.log(event.target.innerHTML);
+
+        //hämtar index från knappen som assosieras med buddyns arrayposition
+        const selectedByddi_Index = event.target.id.replace("buddyIndex:", "");
+        // console.log(byddiIndex);
+
+
+        renderPokemon();
+
+        async function renderPokemon() {
+            //Hämta subpage som tillhär den vaöda pokemon:
+
+            const res = await fetch(buddys[selectedByddi_Index].url);
+            if (res.ok) {
+                // console.log(res);
+                const buddy = await res.json();
+                // console.log(buddy);
+                const buddyName = buddy.name.toUpperCase();
+
+                // renderar ut bild i domen
+                buddy_container.innerHTML = `<img src="${buddy.sprites.front_default}" alt="buddy">`
+
+                //Visar knapp för att låta pokemon återvända:
+                buddyReturn_button.removeAttribute("style");
+                buddyReturn_button.innerHTML = `${buddyName}, återvänd!`
+
+            } else {
+                console.error(res);
+            }
+        };
+
+
+
+
+    }
+});
+
+
+console.log(buddyList_div);
