604f432d59cc49deb1936667459dbb779b4b415a
Viktor Magnusson <viktor.kuddis@hotmail.com>
2023-12-19 04:13:38 +0100

    style and refactor

--- js/buddy.js
+++ js/buddy.js
@@ -1,29 +1,42 @@
 
-/* 
-
-getpokedex():
-Först hämtas alla pokemon och filtreras ner 
-till starters och skickas till arrayen buddys.
-Funktionen anropar i sin tur funktioneen som renderar alternativen till domen
-*/
-
 
 
 const buddyList_div = document.getElementById("buddy-list");
 const buddy_container = document.getElementById("buddy_container");
 const buddyReturn_button = document.getElementById("buddy-return_button");
+const buddySubheadding = document.getElementById("buddy-subheadding")
 
 //Döljer return-button initialt:
 buddyReturn_button.style.display = "none";
 
 
+let buddyName = localStorage.getItem("buddyName") || "";
+let buddyImg = localStorage.getItem("buddyImg") || "";
 
-let buddys = [];
+//boolean:
+buddyIsActive = JSON.parse(localStorage.getItem("buddyIsActive"));
+// console.log(buddyIsActive);
+
+//om det finns en buddy så renderas den
+if (buddyIsActive) {
+    renderBuddy()
+}
 
 
+
+//-------------------------------------------
+
+
+
+/* 
+getpokedex():
+Först hämtas alla pokemon och filtreras ner 
+till starters och skickas till arrayen buddys.
+Funktionen anropar i sin tur funktioneen som renderar alternativen till domen
+*/
 getPokedex();
 
-
+let buddys = [];
 
 async function getPokedex() {
     //anropar pokedex med limit på 151 st
@@ -57,7 +70,7 @@ async function getPokedex() {
             return namesToReturn.includes(pokemon.name);
         });
 
-        console.log(buddys);
+        // console.log(buddys);
 
         renderBudyList();
 
@@ -68,6 +81,9 @@ async function getPokedex() {
 
 
 
+
+
+//renderar de möjliga buddys till domen
 function renderBudyList() {
     let buddyListToRender = buddys.map((pokemon, index) => {
         return `
@@ -78,40 +94,49 @@ function renderBudyList() {
 
 }
 
-//lyssnar efter knapptryck på buddylistan:
+
+
+
+
+
+/* 
+    -lyssnar efter knapptryck på buddylistan för 
+    att hämta merinfo om den valda buddyn.
+    -Lagrar buddyns bild och namn i local storage.
+    Sedan anropar funktionen renderBuddy()
+*/
 buddyList_div.addEventListener("click", (event) => {
-    console.log(event);
+    // console.log(event);
 
     if (event.target.nodeName == "BUTTON") {
         // console.log(event.target.innerHTML);
 
         //hämtar index från knappen som assosieras med buddyns arrayposition
-        const selectedByddi_Index = event.target.id.replace("buddyIndex:", "");
-        // console.log(byddiIndex);
+        const selectedBuddy_Index = event.target.id.replace("buddyIndex:", "");
 
 
-        renderPokemon();
+        //Hämta subpage som tillhör den valda pokemon:
+        fetchPokemon();
+        async function fetchPokemon() {
 
-        async function renderPokemon() {
-            //Hämta subpage som tillhär den vaöda pokemon:
-
-            const res = await fetch(buddys[selectedByddi_Index].url);
+            const res = await fetch(buddys[selectedBuddy_Index].url);
             if (res.ok) {
                 // console.log(res);
                 const buddy = await res.json();
                 // console.log(buddy);
-                const buddyName = buddy.name.toUpperCase();
 
-                // renderar ut bild i domen
-                buddy_container.innerHTML = `<img src="${buddy.sprites.front_default}" alt="buddy">`
 
-                //Visar knapp för att låta pokemon återvända:
-                buddyReturn_button.removeAttribute("style");
-                buddyReturn_button.innerHTML = `${buddyName}, ÅTERVÄND!`
-                //när knappen klickas återvänder pokemonen
-                buddyReturn_button.addEventListener("click", () => {
-                    buddy_container.innerHTML = "";
-                })
+                //Sparar buddyns namn och bild:
+                buddyName = buddy.name.toUpperCase();
+                buddyImg = buddy.sprites.front_default;
+                //skickar nan och bild till local Storage:
+                localStorage.setItem("buddyName", buddyName);
+                localStorage.setItem("buddyImg", buddyImg);
+
+                //Skickar bolean som indikerar på att det finns en buddy vald
+                localStorage.setItem("buddyIsActive", "true");
+
+                renderBuddy();
 
             } else {
                 console.error(res);
@@ -119,10 +144,39 @@ buddyList_div.addEventListener("click", (event) => {
         };
 
 
-
-
     }
 });
 
 
-console.log(buddyList_div);
+
+
+
+
+/* Renderar bild och namn på budy, 
+samt knapp för att återkalla */
+function renderBuddy() {
+
+    // renderar ut bild i domen
+    buddy_container.innerHTML = `<img src="${buddyImg}" alt="buddy">`
+
+    buddySubheadding.innerHTML = ` - ${buddyName}`
+    //Visar knapp för att låta pokemon återvända:
+    buddyReturn_button.removeAttribute("style");
+    buddyReturn_button.innerHTML = `${buddyName}, ÅTERVÄND!`
+
+    //när knappen klickas återvänder pokemonen
+    buddyReturn_button.addEventListener("click", () => {
+
+
+        buddy_container.innerHTML = "";
+        buddySubheadding.innerHTML = ""
+        buddyReturn_button.style.display = "none"
+
+        localStorage.removeItem("buddyName");
+        localStorage.removeItem("buddyImg");
+
+        localStorage.setItem("buddyIsActive", "false");
+
+    })
+
+};
